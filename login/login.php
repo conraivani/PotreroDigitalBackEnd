1529a9ba012f5db15764bad252b58e0ee35edf80
conraivani <107953280+conraivani@users.noreply.github.com>
2023-09-20 17:04:47 -0300

    Add files via upload

---
+++ login/login.php
@@ -0,0 +1,16 @@
+<?php
+$usuario = $_POST ["usuario"];
+$contrasenia = $_POST ["pass"];
+
+
+$ckusuario = "admin";
+$ckpass = "1234";
+
+if ($usuario==$ckusuario & $contrasenia==$ckpass) {
+    header("location: https://www.potrerodigital.org/");
+
+} else {
+    echo "Incorrecto";
+    header("location:https://google.com/error404");
+}
+?>
